74944d87bde5d58f018e01ac689875fc73dd4e64
MuazAlhaidar <omunam10@gmail.com>
2020-10-06 09:04:52 -0400

    Update BMICalculator.cpp
    organized function declaration
    
    added float point precision to BMI output
    
    added empty displayBMICatagory function

--- BMICalculator.cpp
+++ BMICalculator.cpp
@@ -1,15 +1,8 @@
+#include <iomanip>
 #include <iostream>
 
 #define METRIC 1
 #define IMPERIAL 2
-
-int getMetricBMI(const float weight, const float height) {
-    return weight / (height * height);
-}
-
-int getImperialBMI(const float weight, const float height) {
-    return (703 * weight) / (height * height);
-}
 
 bool isPositiveNum(const float value) {
     if (value > 0)
@@ -18,10 +11,33 @@ bool isPositiveNum(const float value) {
     return false;
 }
 
+void clearInputStream() {
+    std::cin.clear();
+    std::cin.ignore(10000, '\n');
+}
+
 void errorMessage() {
     std::cout << "ERROR:NUMBER NOT IN RANGE\n";
     std::cout << "Please enter a Number Greater than 0\n ";
     clearInputStream();
+}
+
+void displayBMICatagory(const float bmi) {
+}
+
+void getWeightAndHeight(const int unitChoice, float &weight, float &height) {
+
+    std::cout << ((unitChoice == METRIC) ? "Please enter weight(kg): " : "Please enter weight(lb):");
+    std::cin >> weight;
+    clearInputStream();
+    if (!isPositiveNum(weight))
+        weight = getCorrectInput(weight);
+
+    std::cout << ((unitChoice == METRIC) ? "Please enter height(m): " : "Please enter height(in):");
+    std::cin >> height;
+    clearInputStream();
+    if (!isPositiveNum(height))
+        height = getCorrectInput(height);
 }
 
 float getCorrectInput(float value) {
@@ -37,24 +53,12 @@ float getCorrectInput(float value) {
     return value;
 }
 
-void clearInputStream() {
-    std::cin.clear();
-    std::cin.ignore(10000, '\n');
+float getMetricBMI(const float weight, const float height) {
+    return weight / (height * height);
 }
 
-void getWeightAndHeight(const int unitChoice, float &weight, float &height) {
-
-    std::cout << ((unitChoice == METRIC) ? "Please enter weight(kg): " : "Please enter weight(lb):");
-    std::cin >> weight;
-    clearInputStream();
-    if (isPositiveNum(weight))
-        weight = getCorrectInput(weight);
-
-    std::cout << ((unitChoice == METRIC) ? "Please enter height(m): " : "Please enter height(in):");
-    std::cin >> height;
-    clearInputStream();
-    if (isPositiveNum(height))
-        height = getCorrectInput(height);
+float getImperialBMI(const float weight, const float height) {
+    return 703 * (weight / (height * height));
 }
 
 int main() {
@@ -64,8 +68,8 @@ int main() {
 
     std::cout << "Welcome to my BMI Calculator\n";
     std::cout << "Please pick your units of measurement:\n";
-    std::cout << "\t1. Imperical\n";
-    std::cout << "\t2. Metric\n";
+    std::cout << "\t1. Metric\n";
+    std::cout << "\t2. Imperial\n";
 
     std::cin >> unitChoice;
 
@@ -77,8 +81,13 @@ int main() {
 
     getWeightAndHeight(unitChoice, weight, height);
 
+    float userBMI = ((unitChoice == METRIC) ? getMetricBMI(weight, height) : getImperialBMI(weight, height));
+
     std::cout << "Your BMI is: ";
-    std::cout << ((unitChoice == METRIC) ? getMetricBMI(weight, height) : getMetricBMI(weight, height)) << std::endl;
+    std::cout << std::setprecision(3);
+    std::cout << userBMI;
+
+    displayBMICatagory(userBMI);
 
     return 0;
 }
